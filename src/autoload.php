4c65846dd8164718abb1eb24592567037c74fcf9
Kévin Gomez <geek63@gmail.com>
2013-07-23 10:51:59 +0200

    Chg: use an anonymous function to handle the autoload

--- src/autoload.php
+++ src/autoload.php
@@ -4,7 +4,8 @@
  * Simple autoloader that follow the PHP Standards Recommendation #0 (PSR-0)
  * @see https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-0.md for more informations.
  */
-function autoload_vex($className) {
+
+spl_autoload_register(function($className) {
     $className = ltrim($className, '\\');
     if (0 === strpos($className, 'Vex')) {
         $fileName = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
@@ -16,6 +17,4 @@ function autoload_vex($className) {
     }
 
     return false;
-};
-
-spl_autoload_register('autoload_vex');
+});
